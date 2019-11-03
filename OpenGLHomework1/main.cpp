2d2f329c9de211adc0c32de28a768ca5cdd2e11a
mrodil <supermollychan@gmail.com>
2019-11-02 23:26:52 -0700

    Implemented generation of pyramid structure and rotation and scaling of the top cube. Cleaned up code.

--- OpenGLHomework1/main.cpp
+++ OpenGLHomework1/main.cpp
@@ -144,31 +144,14 @@ int main( )
                -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
     };
     
-// world space positions of our cubes
-glm::vec3 cubePositions[] = {
-    glm::vec3( 0.0f,  0.0f,  0.0f),
-    glm::vec3( 2.0f,  5.0f, -15.0f),
-    glm::vec3(-1.5f, -2.2f, -2.5f),
-    glm::vec3(-3.8f, -2.0f, -12.3f),
-    glm::vec3( 2.4f, -0.4f, -3.5f),
-    glm::vec3(-1.7f,  3.0f, -7.5f),
-    glm::vec3( 1.3f, -2.0f, -2.5f),
-    glm::vec3( 1.5f,  2.0f, -2.5f),
-    glm::vec3( 1.5f,  0.2f, -1.5f),
-    glm::vec3(-1.3f,  1.0f, -1.5f)
-};
      GLuint VBO, VAO;
        glGenVertexArrays(1, &VAO);
        glGenBuffers(1, &VBO);
-//       glGenBuffers(1, &EBO);
 
        glBindVertexArray(VAO);
 
        glBindBuffer(GL_ARRAY_BUFFER, VBO);
        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-//       glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//       glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
        // position attribute
        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -176,9 +159,6 @@ glm::vec3 cubePositions[] = {
        // color attribute
        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
        glEnableVertexAttribArray(1);
-//       // texture coord attribute
-//       glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-//       glEnableVertexAttribArray(2);
 
 
     // load and create a texture
@@ -238,35 +218,6 @@ glm::vec3 cubePositions[] = {
         
         // activate shader
         ourShader.Use();
-//       // create transformations
-//        glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-//        glm::mat4 view          = glm::mat4(1.0f);
-//        glm::mat4 projection    = glm::mat4(1.0f);
-//        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-//        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-//        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-//        // retrieve the matrix uniform locations
-//        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-//        unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view");
-//        // pass them to the shaders (3 different ways)
-//        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-//        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-//        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
-//        ourShader.setMat4("projection", projection);
-//
-//       // render boxes
-//        glBindVertexArray(VAO);
-//        for (unsigned int i = 0; i < 10; i++)
-//        {
-//            // calculate the model matrix for each object and pass it to shader before drawing
-//            glm::mat4 model = glm::mat4(1.0f);
-//            model = glm::translate(model, cubePositions[i]);
-//            float angle = 20.0f * i;
-//            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-//            ourShader.setMat4("model", model);
-//
-//            glDrawArrays(GL_TRIANGLES, 0, 36);
-//        }
 
         
         // pass projection matrix to shader (note that in this case it could change every frame)
@@ -279,16 +230,39 @@ glm::vec3 cubePositions[] = {
 
         // render boxes
         glBindVertexArray(VAO);
-        for (unsigned int i = 0; i < 10; i++)
+        float cubeSize = 1.0f;
+        float max = 4.0f;
+        float min = 1;
+        
+        for (float  i = 0; i < max; i++)
         {
-            // calculate the model matrix for each object and pass it to shader before drawing
-            glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-            model = glm::translate(model, cubePositions[i]);
-            float angle = 20.0f * i;
-            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            ourShader.setMat4("model", model);
+            for (int j = max-i; j >= min; --j)
+            {
+                for (int k = max-i; k >= min; --k)
+                {
+                    // calculate world space positions of cubes
+                    float offset = i *  cubeSize/2;
+                    float x = (k * cubeSize) + offset;
+                    float z = (j * cubeSize ) + offset;
+                    float y = i * cubeSize;
+                    // calculate the model matrix for each object and pass it to shader before drawing
+                    glm::mat4 model = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+                    if(i == max-1){//if the top cube
+                        
+                        model = glm::translate(model, glm::vec3( x, y, z));
+                        float angle = 45.0f;
+                        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.5f, 0.5f));
+                        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(1.0f, 1.0f, 1.0f));
+                        model = glm::scale(model, glm::vec3(0.6, 0.6, 0.6));
+                    }else{
+                        model = glm::translate(model, glm::vec3( x, y, z));
+                    }
+//
+                                ourShader.setMat4("model", model);
 
-            glDrawArrays(GL_TRIANGLES, 0, 36);
+                                glDrawArrays(GL_TRIANGLES, 0, 36);
+                }
+            }
         }
 
         
